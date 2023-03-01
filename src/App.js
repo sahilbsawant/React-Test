9fd149b2b79fb947290bf22ace963476dc378297
sahilbsawant <sbsawant358@gmail.com>
2023-03-01 20:03:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,152 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+      <div className="align-self-end"> 
+        <div className="row">
+          <div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+                <p className="card-text">company:- card tech </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div><div className="col-md-3">
+            <div class="card">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                class="card-img-top"
+                alt="..."
+              />
+              <h5 className="person1">Rahul Sharma</h5>
+              <div class="card-body">
+                <p className="card-text">Email:- rahul@gmail.com </p>
+                <p className="card-text">Phone:- 6483652813 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        
+</>
+  );
+};
+
+export default App;
